04721ec43ee80046e8c8743e74aeb0573926f3d0
smankah <95653107+smankah@users.noreply.github.com>
2025-03-11 12:21:19 -0400

    Update index.js
    Enhanced PR check

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,22 @@ exports.index = function (req, res, next) {
     });
 };
 
-
+exports.loginHandlerTest = function (req, res, next) {
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
 exports.admin = function (req, res, next) {
   console.log(req.body);
   User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
